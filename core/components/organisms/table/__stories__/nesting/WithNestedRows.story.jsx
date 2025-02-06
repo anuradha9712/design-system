8b8ef859da46ee4e484e1742e2aa410f355c92f1
anuradha9712 <anuradha.mcs18.du@gmail.com>
2025-02-06 17:47:42 +0400

    feat(table): add support to expand nested rows in table

--- core/components/organisms/table/__stories__/nesting/WithNestedRows.story.jsx
+++ core/components/organisms/table/__stories__/nesting/WithNestedRows.story.jsx
@@ -130,6 +130,74 @@ const customCode = `
     },
   ];
 
+  const nestedRowSchema = [
+    {
+      name: 'name',
+      displayName: 'Name',
+      width: 300,
+      resizable: true,
+      separator: true,
+      tooltip: true,
+      translate: a => ({
+        title: \`\${a.firstName} \${a.lastName}\`,
+        firstName: a.firstName,
+        lastName: a.lastName
+      }),
+      filters: [
+        { label: 'A-G', value: 'a-g' },
+        { label: 'H-R', value: 'h-r' },
+        { label: 'S-Z', value: 's-z' },
+      ],
+      onFilterChange: (a, filters) => {
+        for (const filter of filters) {
+          switch (filter) {
+            case 'a-g':
+              if (a.firstName[0].toLowerCase() >= 'a' && a.firstName[0].toLowerCase() <= 'g') return true;
+              break;
+            case 'h-r':
+              if (a.firstName[0].toLowerCase() >= 'h' && a.firstName[0].toLowerCase() <= 'r') return true;
+              break;
+            case 's-z':
+              if (a.firstName[0].toLowerCase() >= 's' && a.firstName[0].toLowerCase() <= 'z') return true;
+              break;
+          }
+        }
+        return false;
+      },
+      cellType: 'AVATAR_WITH_TEXT',
+    },
+    {
+      name: 'email',
+      displayName: 'Email',
+      width: 350,
+      resizable: true,
+      sorting: false,
+      cellType: 'WITH_META_LIST'
+    },
+    {
+      name: 'gender',
+      displayName: 'Gender',
+      width: 200,
+      resizable: true,
+      comparator: (a, b) => a.gender.localeCompare(b.gender),
+      cellType: 'STATUS_HINT',
+      translate: a => ({
+        title: a.gender,
+        statusAppearance: (a.gender === 'Female') ? 'alert' : 'success'
+      }),
+      filters: [
+        { label: 'Male', value: 'male' },
+        { label: 'Female', value: 'female' },
+      ],
+      onFilterChange: (a, filters) => {
+        for (const filter of filters) {
+          if (a.gender.toLowerCase() === filter) return true;
+        }
+        return false;
+      },
+    },
+  ];
+
   const nestedRowRenderer = (props) => {
     const {
       schema,
@@ -147,8 +215,8 @@ const customCode = `
         <Divider className='ml-5' />
         <List
           loading={loading}
-          schema={schema}
-          data={[data]}
+          schema={nestedRowSchema}
+          data={data._nestedRows}
         />
       </div>
     );
